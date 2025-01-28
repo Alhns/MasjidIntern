4916c297d06f56d444d503415c485e932d542e67
alhns <151588068+Alhns@users.noreply.github.com>
2025-01-29 01:30:52 +0800

    Update signIn.js

--- static/js/signIn.js
+++ static/js/signIn.js
@@ -1,15 +1,4 @@
-document.getElementById('signInButton').addEventListener('click', function () {
-    const masjidIdInput = document.getElementById('masjidId');
-    const errorMessage = document.getElementById('errorMessage');
-
-    if (masjidIdInput.value.trim() === '') {
-        errorMessage.style.display = 'block';
-    } else {
-        errorMessage.style.display = 'none';
-        window.location.href = 'next-page.html';
-    }
-});
-
-document.getElementById('signUpButton').addEventListener('click', function () {
-    alert('Redirecting to the Sign-Up page.');
-});
+// Redirects user to the signup page
+function redirectToSignup() {
+    window.location.href = 'signup.php';
+}
